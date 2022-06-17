986f02512687cf64f1fb13413af67fab2543a0d6
santosh33 <santosh33@gmail.com>
2022-06-17 10:44:03 +0530

    Created App Component for axios pkg  list of img

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+
+class App extends React.Component{
+    state = { images: []};
+    
+    async onSearchSubmit(term){
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
+            params:{query:term},
+            headers:{
+                Authorization:'Client-ID QSpQ5X9Q87eHIcDQsAE8sbpIZd0CKIonmD3bTKuwZkI'
+            }
+        });
+
+        console.log(response.data.results);
+    }
+
+    render(){
+        return (
+            <div className='ui container' style={{marginTop:'10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+            );
+    }
+}
+
+export default App;
